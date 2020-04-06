d9150a39caa37ed1ecdb36f6a05590ff1a50b5e6
Amy Franz <amy.franz@gmail.com>
2020-04-06 12:58:45 +0100

    Done.

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,23 +1,71 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import Search from './Search'
-import { Container } from 'semantic-ui-react'
+import React from "react";
+import PokemonCollection from "./PokemonCollection";
+import PokemonForm from "./PokemonForm";
+import Search from "./Search";
+import { Container } from "semantic-ui-react";
 
 class PokemonPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemon: "",
+      searchTerm: "",
+    };
+    this.fetchPokemon();
+  }
+
+  fetchPokemon = () => {
+    fetch("http://localhost:3000/pokemon")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ pokemon: data });
+      });
+  };
+
   render() {
+    const pokemonsSearch = this.state.searchTerm
+      ? this.state.pokemon.filter((pokemon) =>
+          pokemon.name.includes(this.state.searchTerm)
+        )
+      : this.state.pokemon;
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm handleSubmit={this.handleSubmit} />
         <br />
-        <Search onChange={() => console.log('🤔')} />
+        <Search
+          onChange={(e) => this.setState({ searchTerm: e.target.value })}
+        />
         <br />
-        <PokemonCollection />
+        {this.state.pokemon ? (
+          <PokemonCollection pokemons={pokemonsSearch} />
+        ) : null}
       </Container>
-    )
+    );
   }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    debugger;
+
+    const body = {
+      name: e.target.name.value,
+      stats: [{ name: "hp", value: e.target.hp.value }],
+      sprites: { front: e.target.frontUrl.value, back: e.target.backUrl.value },
+    };
+    fetch("http://localhost:3000/pokemon", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        accept: "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        this.fetchPokemon();
+      });
+  };
 }
 
-export default PokemonPage
+export default PokemonPage;
